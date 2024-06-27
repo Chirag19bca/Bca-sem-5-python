b8c843a192a7594e7dbe243f02928237defc9b41
Chirag19bca <119275895+Chirag19bca@users.noreply.github.com>
2024-06-27 20:06:30 +0530

    27 jan python programs 1 to 8

--- college/UNIT-01/06.py
+++ college/UNIT-01/06.py
@@ -1,10 +1,8 @@
 a=int(input("enter no: "))
 if a>1:
-    for i in range(2,(a//2)+1):
+    for i in range(2,a):
         if (a%i)==0:
             print(a," is not prime number")
             break
     else:
         print(a," is prime number")
-else:
-    print(a," is not prime number")
